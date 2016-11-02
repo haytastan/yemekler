c2eb9fa9062cf4092cd599a6d4348b64b8f4241f
agtokty <agitoktay@gmail.com>
2016-11-02 09:17:34 +0300

    ay isimleri farklı gelebiliyor

--- Apps/YYYemekhane/app/src/main/java/com/agtokty/YemekhaneUtil.java
+++ Apps/YYYemekhane/app/src/main/java/com/agtokty/YemekhaneUtil.java
@@ -10,27 +10,27 @@ public class YemekhaneUtil {
 
         if (ay.equals("Ocak"))
             return 1;
-        else if (ay.equals("Subat"))
+        else if (ay.equals("Subat") || ay.equals("Şubat"))
             return 2;
         else if (ay.equals("Mart"))
             return 3;
-        else if (ay.equals("Nisan"))
+        else if (ay.equals("Nisan") || ay.equals("Nısan"))
             return 4;
-        else if (ay.equals("Mayis"))
+        else if (ay.equals("Mayis") || ay.equals("Mayıs"))
             return 5;
-        else if (ay.equals("Haziran"))
+        else if (ay.equals("Haziran") || ay.equals("Hazıran"))
             return 6;
         else if (ay.equals("Temmuz"))
             return 7;
-        else if (ay.equals("Agustos"))
+        else if (ay.equals("Agustos") || ay.equals("Ağustos"))
             return 8;
-        else if (ay.equals("Eylül"))
+        else if (ay.equals("Eylül") || ay.equals("Eylul"))
             return 9;
-        else if (ay.equals("Ekim"))
+        else if (ay.equals("Ekim") || ay.equals("Ekım"))
             return 10;
-        else if (ay.equals("Kasim"))
+        else if (ay.equals("Kasim") || ay.equals("Kasım"))
             return 11;
-        else if (ay.equals("Aralik"))
+        else if (ay.equals("Aralik") || ay.equals("Aralık"))
             return 12;
         else
             return 13;
